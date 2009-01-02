665b8575dbf21cc50bd476f26a81f5939a80194a
Joe Groff <arcata@gmail.com>
2009-01-01 18:11:04 -0800

    force cocoa to be thread-safe

--- main.m
+++ main.m
@@ -1,8 +1,18 @@
 #import <UIKit/UIKit.h>
 
+@interface BattleMintsDummy : NSObject {}
+- (void)whatever:(id)foo;
+@end
+@implementation BattleMintsDummy : NSObject {}
+- (void)whatever:(id)foo { [NSThread exit]; }
+@end
+
 int main(int argc, char *argv[])
 {    
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    [NSThread detachNewThreadSelector:@selector(whatever:)
+              toTarget:[[[BattleMintsDummy alloc] init] autorelease]
+              withObject:nil];
     int retVal = UIApplicationMain(argc, argv, nil, nil);
     [pool release];
     return retVal;
