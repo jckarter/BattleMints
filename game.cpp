9c0153c32abfc0547552104dbf41bb096c938609
Joe Groff <arcata@gmail.com>
2009-01-01 21:43:40 -0800

    tweaks

--- game.cpp
+++ game.cpp
@@ -17,8 +17,9 @@ const vec2 GAME_WINDOW_UNIT_SIZE = make_vec2(320.0, 480.0) * GAME_UNITS_PER_PIXE
 const vec2 board::COLLISION_CELL_SIZE = make_vec2(2.0, 2.0);
 const vec2 board::VISIBILITY_CELL_SIZE = make_vec2(2.0, 2.0);
 
-const float camera::LEAD_FACTOR = 20.0f;
+const float camera::LEAD_FACTOR = 15.0f;
 const float camera::FOLLOW_FACTOR = 0.1f;
+const float camera::ACCEL = 0.03f;
 
 const float player::ACCEL_SCALE = 0.02f;
 const float player::RADIUS = 0.5f;
@@ -28,9 +29,9 @@ const vec4 player::COLOR = make_vec4(0.5, 0.89, 1.0, 1.0);
 
 const float mini::ACCEL = 0.022f;
 const float mini::RADIUS = 0.35f;
-const float mini::MASS = 0.6f;
+const float mini::MASS = 0.4f;
 const float mini::SPRING = 1.0f;
-const float mini::RESPONSIVENESS = 0.75f;
+const float mini::RESPONSIVENESS = 0.65f;
 
 const float mega::ACCEL = 0.006f;
 const float mega::RADIUS = 2.6f;
