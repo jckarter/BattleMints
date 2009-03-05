c0420c5ccbf283f965ac9170534c26de2c4788c1
Joe Groff <arcata@gmail.com>
2009-03-04 21:28:51 -0600

    start on hub.board rather than test.board

--- intro.cpp
+++ intro.cpp
@@ -54,7 +54,7 @@ void
 intro::tick()
 {
     if (_countdown == 0)
-        controller::set_current(board::from_file("test"));
+        controller::set_current(board::from_file("hub"));
     else
         --_countdown;
 }
